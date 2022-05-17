6f1401793c548740d1e4ff9abdc4143bd40da7b7
tony <apol.lun058@gmail.com>
2022-05-17 12:13:22 +0900

    add:exception

--- job_task/views.py
+++ job_task/views.py
@@ -7,15 +7,37 @@ from rest_framework.response import Response
 from .job_utils import CSVController, JobController
 
 
-# job이 하나도 없는 상태에서 JSONDecodeError 에러발생 예외처리 해줘야함
 class JobTaskSave(APIView):
     def post(self, request):
+        # job 생성
         data = request.data
-        job = JobController().get_job_data()
-        keys = job.keys()
 
-        # auto increment
-        pk = int(list(keys)[-1]) + 1
+        # job 생성에서 직접 id를 입력하려 한 경우
+        if data.get('job_id'):
+            return Response({"message" : "job_id는 직접 입력 할 수 없습니다."},
+                status=status.HTTP_400_BAD_REQUEST
+                )
+
+        # 필수값이 안들어온 경우
+        if list(data.keys()) != ['job_name', 'task_list', 'property']:
+            return Response({"message" : "필수값을 확인하세요. ['job_name', 'task_list', 'property']"},
+                status=status.HTTP_400_BAD_REQUEST
+                )
+
+        try:
+            job = JobController().get_job_data()
+            # pk auto increment
+            keys = job.keys()
+            pk = int(list(keys)[-1]) + 1
+
+            # 필수값이 안들어온 경우
+            if list(data.keys()) != ['job_name', 'task_list', 'property']:
+                raise KeyError
+
+        except (FileNotFoundError, JSONDecodeError):
+            # job.json 파일이 존재하지 않거나, job.json 파일이 비어있을 경우
+            pk = "1"
+            job = {}
 
         job[f"{pk}"] = JobController().get_json_from(pk, data)
         result = JobController().set_job_data(job)
@@ -25,7 +47,11 @@ class JobTaskSave(APIView):
 class JobTaskEdit(APIView):
     def delete(self, request, pk):
         job = JobController().get_job_data()
-        del job[f'{pk}']
+
+        if pk not in list(job.keys()):
+            return Response({"message" : "존재하지 않는 job_id입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+        del job[pk]
         result = JobController().set_job_data(job)
         return Response(result, status=status.HTTP_204_NO_CONTENT)
 
@@ -33,13 +59,19 @@ class JobTaskEdit(APIView):
         data = request.data
         job = JobController().get_job_data()
 
-        job[f'{pk}'] = JobController().get_json_from(pk, data)
+        if pk not in list(job.keys()):
+            return Response({"message" : "존재하지 않는 job_id입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+        job[pk] = JobController().get_json_from(pk, data)
         result = JobController().set_job_data(job)
         return Response(result, status=status.HTTP_200_OK)
 
     def get(self, request, pk):
         job = JobController().get_job_data()
-        job = job[f'{pk}']
+        job = job[pk]
+
+        if pk not in list(job.keys()):
+            return Response({"message" : "존재하지 않는 job_id입니다."}, status=status.HTTP_400_BAD_REQUEST)
 
         task_list = JobController().topology_sort(job['task_list'])
 
